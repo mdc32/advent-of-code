eb7306bc1dbdd63556d82385853a41ec24b38bc3
Miles Christensen <9355108+mdc32@users.noreply.github.com>
2021-12-09 12:03:46 -0800

    day 3 part 1

--- 2021/Day3/miles.py
+++ 2021/Day3/miles.py
@@ -1,7 +1,21 @@
 from helpers import *
+from functools import reduce
+
 
 def part1(filename: str) -> int:
-  return -1
+  lines = [list(map(int, list(x))) for x in to_lines(filename)]
+  l = len(lines[0])
+  print([mcv([x[i:i+1] for x in lines]) for i in range(l)])
+  v = int(''.join([str(mcv([x[i:i+1] for x in lines])) for i in range(l)]),2)
+  return v* (2**l-v-1)
+
+
 
 def part2(filename: str) -> int:
-  return -1
+  lines = to_lines(filename)
+
+def mcv(lines):
+  return 1 if sum([int(x[0]) for x in lines]) >= len(lines)/2 else 0
+
+def lcv(lines):
+  return 0 if sum([int(x[0]) for x in lines]) >= len(lines)/2 else 1
