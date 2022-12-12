ed44419ffce42b4cd23ccf2e44fa3a20b2e01628
Miles Christensen <9355108+mdc32@users.noreply.github.com>
2022-12-11 22:37:09 -0800

    finish through day 9

--- 2022/Day8/miles.py
+++ 2022/Day8/miles.py
@@ -1,7 +1,41 @@
 from helpers import *
+import numpy as np
 
 def part1(filename: str) -> int:
-  pass #TODO: complete
+  g = np.array([[int(c) for c in l] for l in to_lines(filename)])
+  w,h = len(g[0]), len(g)
+  m1 = np.array([f(l) for l in g])
+  m2 = np.array([f(l) for l in g.T]).T
+  m3 = np.fliplr(np.array([f(l) for l in np.fliplr(g)]))
+  m4 = np.flipud(np.array([f(l) for l in np.flipud(g).T]).T)
+  return np.sum(1 - m1*m2*m3*m4)
+  
 
 def part2(filename: str) -> int:
-  pass #TODO: complete
+  g = np.array([[int(c) for c in l] for l in to_lines(filename)])
+  w,h = len(g[0]), len(g)
+  m1 = np.array([v(l) for l in g])
+  m2 = np.array([v(l) for l in g.T]).T
+  m3 = np.fliplr(np.array([v(l) for l in np.fliplr(g)]))
+  m4 = np.flipud(np.array([v(l) for l in np.flipud(g).T]).T)
+  return np.amax(m1*m2*m3*m4)
+
+
+def f(l):
+  m = [1]*len(l)
+  h = -1
+  for i in range(len(l)):
+    if l[i] > h:
+      h = l[i]
+      m[i] = 0
+  return m
+
+def v(l):
+  m = [0]*len(l)
+  for i in range(1,len(l)):
+    k = i-1
+    while l[k]<l[i] and k>0:
+      k -= m[k]
+    m[i] = i-k
+  return m
+    
--- 2022/Day9/miles.py
+++ 2022/Day9/miles.py
@@ -1,7 +1,78 @@
 from helpers import *
 
 def part1(filename: str) -> int:
-  pass #TODO: complete
+  l = to_lines(filename)
+  v = {(0,0)}
+  hx,hy,tx,ty = 0,0,0,0
+  for i in l:
+    d,a = i.split()
+    for k in range(int(a)):
+      if d=="R":
+        hx+=1
+      elif d=="L":
+        hx-=1
+      elif d=="U":
+        hy+=1
+      else:
+        hy-=1
+      tx,ty = pull((hx,hy),(tx,ty))
+      v.add((tx,ty))
+  return len(v)
 
 def part2(filename: str) -> int:
-  pass #TODO: complete
+  l = to_lines(filename)
+  v = {(0,0)}
+  p = [(0,0)]*10
+  for i in l:
+    d,a = i.split()
+    for k in range(int(a)):
+      hx,hy = p[0]
+      if d=="R":
+        hx+=1
+      elif d=="L":
+        hx-=1
+      elif d=="U":
+        hy+=1
+      else:
+        hy-=1
+      p[0] = (hx,hy)
+      for j in range(1,len(p)):
+        p[j] = pull(p[j-1], p[j])
+      # grid(p)
+      v.add(p[-1])
+  return len(v)
+
+def pull(h,t):
+  hx,hy = h
+  tx,ty = t
+  xd,yd = abs(hx-tx), abs(hy-ty)
+  if xd == 2:
+    tx = (hx+tx)//2
+    if yd == 2:
+      ty = (hy+ty)//2
+    else:
+      ty=hy
+  elif yd ==2:
+    ty = (hy+ty)//2
+    tx=hx
+  return (tx,ty)
+
+def grid(p):
+  maxx = max(i[0] for i in p)
+  minx = min(i[0] for i in p)
+  maxy = max(i[1] for i in p)
+  miny = min(i[1] for i in p)
+  dx,dy = maxx-minx+1, maxy-miny+1
+  g = ["."*(dx+1)]*(dy+1)
+  for i in range(len(p)):
+    x,y = p[i]
+    g[y] = g[y][:x]+str(i)+g[y][x+1:]
+
+  print("="*10)
+  for y in range(dy-1,-1,-1):
+    print(g[y])
+  print("="*10)
+
+
+  
+
