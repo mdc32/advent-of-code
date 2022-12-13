70788343e0417ebc3aee2cf218a0a3690a2f6c41
Miles Christensen <9355108+mdc32@users.noreply.github.com>
2022-12-12 22:08:34 -0800

    rename directories and update advent.py

--- advent.py
+++ advent.py
@@ -1,23 +1,22 @@
 import importlib
 import argparse, sys
-from datetime import datetime
-
+from datetime import datetime, timezone, timedelta
 if __name__ == "__main__":
 
-  now = datetime.now()
+  now = datetime.now(tz=timezone(timedelta(hours=-5)))
   # i am lazy and this changes to eastern from pacific
-  day = now.day + (0 if now.hour < 21 else 1)
+  # day = now.day + (0 if now.hour < 21 else 1)
 
   # set up command line arguments
   parser=argparse.ArgumentParser()
   parser.add_argument('-n', '--name', default="miles", help="run NAME.py (default = miles)")
-  parser.add_argument('-d', '--day', default=day)
-  parser.add_argument('-y', '--year', default=now.year)
+  parser.add_argument('-d', '--day', default=now.day, type=int)
+  parser.add_argument('-y', '--year', default=now.year, type=int)
   parser.add_argument('-t', '--test', help="run test instead of input",
     action='store_true')
   args=parser.parse_args()
 
-  path = f'{args.year}/Day{args.day}/'
+  path = f'{args.year}/Day{args.day:02}/'
 
   # import module for given day and year, with today (eastern) as default
   module = importlib.import_module(f'{path.replace("/",".")}{args.name}')
